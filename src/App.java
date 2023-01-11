d890d72a57e2fabd5fd39dd48d7d0cdd9bfee1fc
Flavia <flavia.bulad@gmail.com>
2023-01-11 15:09:42 -0300

    add solution

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Student student = new Student();
+
+        student.setName("Flavia Bulad");
+        student.setEmail("flaviabulad@email.com");
+        student.getAddress().setStreet("Rua 1");
+        student.getAddress().setCity("São Paulo");
+        student.getAddress().setState("SP");
+        student.getAddress().setZip("12345-678");
+        student.getAddress().setCountry("Brasil");
+        student.getCourse().setName("Java");
+        student.getCourse().setMentor("Frank Rocha");
+
+        System.out.println("");
+        System.out.println("Responde aí - dados de um aluno:");
+        System.out.println("\n Nome: " + student.getName() + "\n Email: " + student.getEmail() + "\n Endereço: " + student.getAddress().getStreet() + ", " + student.getAddress().getCity() + ", " + student.getAddress().getState() + ", " + student.getAddress().getZip() + ", " + student.getAddress().getCountry() + "\n Curso: " + student.getCourse().getName() + "\n Mentor: " + student.getCourse().getMentor());
+        
     }
 }
---
+++ src/Course.java
@@ -0,0 +1,21 @@
+public class Course {
+    private String name;
+    private String mentor;
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMentor() {
+        return mentor;
+    }
+
+    public void setMentor(String mentor) {
+        this.mentor = mentor;
+    }
+
+}
